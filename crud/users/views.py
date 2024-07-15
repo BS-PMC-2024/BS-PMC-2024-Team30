2844f256999d4404df7038330a52a48a0139f8a8
noamsin2 <noamsin2@gmail.com>
2024-07-15 22:55:19 +0300

    basic website - BSPMS2430-2 , BSPMS2430-3

---
+++ crud/users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
