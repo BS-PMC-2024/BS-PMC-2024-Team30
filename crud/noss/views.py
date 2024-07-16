7b3e038236904d14f7f1270847de2a9b3b34b208
Omer Shay <omershay1212@gmail.com>
2024-07-16 20:44:02 +0300

    test

--- crud/noss/views.py
+++ crud/noss/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
 def home(request):
+<<<<<<< Updated upstream
     return render(request, 'noss/home.html')
 #test
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+    return HttpResponse('<h1>Home</h1>')
+=======
+=======
+>>>>>>> 42c11bd (commit)
+    return render(request, 'noss/home.html')
+
+def login(request):
+    return render(request, 'noss/login.html')
+<<<<<<< HEAD
+>>>>>>> 2844f25 (basic website - BSPMS2430-2 , BSPMS2430-3)
+=======
+=======
+    return HttpResponse('<h1>Home</h1>')
+#set
+>>>>>>> 6e16e92 (commit)
+>>>>>>> 42c11bd (commit)
+>>>>>>> Stashed changes
